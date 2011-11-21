86406da17b30390cf933e6c205fd457d9fbb00f7
Kir <shatrov@me.com>
2011-11-21 14:02:07 +0400

    Rails Engine fix

--- lib/inboxes/engine.rb
+++ lib/inboxes/engine.rb
@@ -2,13 +2,8 @@ require "inboxes/ability"
 
 module Inboxes
   class Engine < ::Rails::Engine
-    # raise "Engine Activated"
     def self.activate
       Ability.register_ability(InboxesAbility)
-      # raise "Activated"
-    end
-    
-    def load_tasks
     end
     
     config.to_prepare &method(:activate).to_proc
